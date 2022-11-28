072fee09f38991901b7b092ab3392dc85cf9cdae
Dmitriy <Wonderful_days@mail.ru>
2022-11-28 17:33:18 +0300

    Siminar2 add first task

--- Siminar2/First/Program.cs
+++ Siminar2/First/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+int Prompt(string message)
+{
+System.Console.Write($"{message} >");
+int answer = Convert.ToInt32(Console.ReadLine());
+return answer;
+}
+
+bool ValidateNumber(int number)
+{
+if (number >= 100 && number < 1000)
+{
+return true;
+}
+
+System.Console.WriteLine("Число не трехзначное");
+return false;
+}
+
+int number = Prompt("Введите число");
+if (ValidateNumber(number))
+{
+int result = number % 10;
+System.Console.WriteLine($"Последняя цифра введенного числа {result}");
+}
