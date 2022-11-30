d1638a9d885439e91097386adfd4d785427d78d2
Dmitriy <Wonderful_days@mail.ru>
2022-11-30 17:43:33 +0300

    Homework2 add first task

---
+++ Homework2/FirstTask/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Не использовать строки
+
+System.Console.WriteLine("Введите трехзначное число >");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while(true)
+{
+    if (number < 100 || number >= 1000)
+    {
+        System.Console.WriteLine("Неверно, введите еще раз:");
+        number = Convert.ToInt32(Console.ReadLine());
+    }
+    else
+    {
+        int chislo = (number / 10) % 10;
+        System.Console.WriteLine($"Вторая цифра равна {chislo}");
+        break;
+    }
+}
