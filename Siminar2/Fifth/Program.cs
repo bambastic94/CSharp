d33de0e10fe154b5ae5dbd68ef51ec0f02aa1d96
Dmitriy <Wonderful_days@mail.ru>
2022-11-30 14:18:51 +0300

    Add fifth task

---
+++ Siminar2/Fifth/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 9999] и показывает наибольшую цифру числа.
+// 7812 -> 8
+// 1213-> 3
+// 845 -> 8
+
+int proverka(int ostatok)
+{
+    int dop = (ostatok / 10) % 10;
+    return dop;
+}
+
+int number = new Random().Next(10, 10000);
+int chislo = number;
+
+int max = number % 10;
+int k = 2;
+int i = 1;
+
+if (number > 100 & number < 1000)   k = 3;
+if (number > 1000 & number < 10000) k = 4;
+
+while( i < k )
+{
+    if (proverka(number) > max)
+    {
+        max = proverka(number);
+    }
+    number = number / 10;
+    i++;
+}
+
+System.Console.WriteLine($"В числе {chislo} самая большая цифра {max}");
