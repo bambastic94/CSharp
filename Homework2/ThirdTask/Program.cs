b7992b41276e8fd0d1ca7cd5f85ba7169facbe9c
Dmitriy <Wonderful_days@mail.ru>
2022-12-01 15:35:24 +0300

    Homework2 add third task

---
+++ Homework2/ThirdTask/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. . Не использовать строки
+
+System.Console.Write("Input number >");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int third = 0;
+int num = number;
+
+while (true)
+{
+    if (number < 100)
+    {
+        System.Console.WriteLine("Invalid number, tape another one:");
+        number = Convert.ToInt32(Console.ReadLine());
+        num = number;
+    }
+    else
+    {
+        while (number >= 1000)
+        {
+            number = number / 10;
+        }
+        third = number % 10;
+        break;
+    }
+}
+
+System.Console.WriteLine($"In number {num} third numeral is {third}");
