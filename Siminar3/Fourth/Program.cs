befdbce20c1af5e8d180870e09cceac7207195db
Dmitriy <Wonderful_days@mail.ru>
2022-12-02 18:55:59 +0300

    Siminar3 add fourth task

---
+++ Siminar3/Fourth/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+int Promt(string massage)
+{
+    Console.Write($"{massage} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int start = 1;
+
+int number = Promt("Введите число");
+
+if (number < 0)
+{
+    number = number * (-1);
+}
+
+while (start <= number)
+{
+    System.Console.Write($"{Math.Pow(start, 2)} ");
+    start++;
+}
