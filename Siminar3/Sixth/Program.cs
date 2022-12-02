7995637bce64badc56dd1bac6b402d31304f7ea5
Dmitriy <Wonderful_days@mail.ru>
2022-12-02 20:07:00 +0300

    Siminar3 add sixth task

---
+++ Siminar3/Sixth/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая выводит первые заданные элемента следующей последовательности:
+// -> 12
+// 1 2 2 3 3 3 4 4 4 4 5 5
+
+int Promt(string massage)
+{
+    Console.Write($"{massage} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int length = Promt("Введите число");
+int counter = 1;
+int counterInside = 1;
+int number = 1;
+
+while (counter <= length)
+{
+    while (counterInside <= number && counter <= length)
+    {
+        System.Console.Write($"{number} ");
+        counterInside++;
+        counter++;
+    }
+    counterInside = 1;
+    number++;
+}
