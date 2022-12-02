6ffc6b4a7b8be45c8ab9cb5f94fe273db817fa91
Dmitriy <Wonderful_days@mail.ru>
2022-12-02 18:48:39 +0300

    Siminar3 add third task

---
+++ Siminar3/Third/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+int Promt(string massage)
+{
+    Console.Write($"{massage} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int coordX1 = Promt("X1=");
+int coordY1 = Promt("Y1=");
+int coordX2 = Promt("X2=");
+int coordY2 = Promt("Y2=");
+
+double distance = Math.Sqrt(Math.Pow(coordX2-coordX1, 2)+Math.Pow(coordY2-coordY1, 2));
+
+System.Console.WriteLine($"Расстояние между точками {distance:F2}");
