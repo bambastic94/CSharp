67c9a67ec838fc690e6ec4ae4ce566e95735d94c
Dmitriy <Wonderful_days@mail.ru>
2022-12-02 19:33:00 +0300

    Siminar3 add fifth task

---
+++ Siminar3/Fifth/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая генерирует последовательность случайных чисел из 10 элементов в диапазоне от 1 до 10, 
+// и подсчитывает, сколько сгенерировалось чисел больше 5.
+
+int[] numArray = new int[10];
+int high = 0;
+
+System.Console.WriteLine("Матрица:");
+
+for (int i = 0; i < numArray.Length; i++)
+{
+    int tmp = new Random().Next(1, 11);
+    numArray[i] = tmp;
+    if (numArray[i] > 5)
+    {
+        high++;
+    }
+    System.Console.Write($"{numArray[i]} ");
+}
+
+System.Console.WriteLine($"Чисел больше 5 - {high}");
