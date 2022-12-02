8e52bc1e45e856d35cfc33d95c6ea8d6495199cf
Dmitriy <Wonderful_days@mail.ru>
2022-12-02 17:03:07 +0300

    Siminar3 add second task

---
+++ Siminar3/Second/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y)
+
+
+int Promt(string massage)
+{
+    Console.Write($"{massage} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int quarter = Promt("Введите четверть");
+
+switch (quarter)
+{
+    case 1:System.Console.WriteLine("Диапазон значений X>0,Y>0");break;
+    case 2:System.Console.WriteLine("Диапазон значений X<0,Y>0");break;
+    case 3:System.Console.WriteLine("Диапазон значений X<0,Y<0");break;
+    case 4:System.Console.WriteLine("Диапазон значений X>0,Y<0");break;
+    default: System.Console.WriteLine("Не соответствует номеру четверти!"); break;
+}
