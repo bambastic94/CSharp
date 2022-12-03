3d543451a3fa649ff2c61571b1912974c49bc36a
Dmitriy <Wonderful_days@mail.ru>
2022-12-03 03:32:18 +0300

    Homework3 add third task

---
+++ Homework3/ThirdTask/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+System.Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+{
+    System.Console.Write($"{Math.Pow(i, 3)} ");
+}
