fe3704d4227509da43acb571fdb323c0b76c2ea6
Dmitriy <Wonderful_days@mail.ru>
2022-12-09 22:37:30 +0300

    Homework4 add first task

---
+++ Homework4/FirstTask/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Реализовать через функции.
+
+int Promt(string massage)
+{
+    Console.Write($"{massage} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int power(int one, int two)
+{
+    int number = one;
+    for (int i = 1; i < two; i++)
+    {
+        number = number * one;
+    }
+    if(two == 0)
+    {
+        number = 1;
+    }
+    return number;
+}
+
+int one = Promt("Input A");
+int two = Promt("Input B");
+
+System.Console.WriteLine($"Power of number is {power(one,two)}");
