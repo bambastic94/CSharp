ede421bef321d048163b5ce7baa5e85c63ed44d6
Dmitriy <Wonderful_days@mail.ru>
2022-12-09 18:57:49 +0300

    Siminar3 add first task

---
+++ Siminar4/First/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+
+int Promt(string massage)
+{
+    Console.Write($"{massage} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = Promt("Введите число: ");
+int count = 0;
+
+int Counter(int number)
+{
+    while (number > 0)
+    {
+        number /= 10;
+        count++;
+    }
+    return count;
+}
+
+System.Console.WriteLine($"Количество цифр в числе {number} - {Counter(number)}");
