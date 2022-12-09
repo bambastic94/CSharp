706bc17aa4a16d4d5bd4fe0e97859eb3ee218f24
Dmitriy <Wonderful_days@mail.ru>
2022-12-09 19:32:17 +0300

    Siminar4 add third task

---
+++ Siminar4/Third/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+
+int Promt(string massage)
+{
+    Console.Write($"{massage} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] GenerateArray(int length, int minrange, int maxrange)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(minrange, maxrange + 1); ;
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        System.Console.Write($"{array[j]} ");
+    }
+}
+
+int length = Promt($"Введите длину массива");
+int minrange = Promt($"Введите минимальную цифру для образования массива");
+int maxrange = Promt($"Введите максимальную цифру для образования массива");
+
+int[] array = GenerateArray(length,minrange,maxrange);
+PrintArray(array);
