929ab28c300a37ccccda322691e891eb6d70b2ee
Dmitriy <Wonderful_days@mail.ru>
2022-12-09 22:45:24 +0300

    Homework4 add second task

---
+++ Homework4/SecondTask/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. Реализовать через функции.
+
+int Promt(string massage)
+{
+    Console.Write($"{massage} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int total(int number)
+{
+    int sum = 0;
+    int temp = 0;
+    while(number>0)
+    {
+        temp = number % 10;
+        sum += temp;
+        number /= 10;
+    }
+    return sum;
+}
+
+int number = Promt("Input number");
+
+System.Console.WriteLine($"Sum of number is {total(number)}");
