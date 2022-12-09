44396704f1035666dbc984fe5f574a6df8d63b70
Dmitriy <Wonderful_days@mail.ru>
2022-12-10 00:19:24 +0300

    Siminar5 add second task

---
+++ Siminar5/Second/Program.cs
@@ -0,0 +1,46 @@
+﻿// Напишите программу замены элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+int Promt(string massage)
+{
+    Console.Write($"{massage} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] GenerateArray(int length, int minrange, int maxrange)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(minrange, maxrange + 1); ;
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        System.Console.Write($"{array[j]}\t");
+    }
+}
+
+int[] Invert(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * (-1);
+    }
+    return array;
+}
+
+int length = Promt($"Введите длину массива");
+int minrange = Promt($"Введите минимальную цифру для образования массива");
+int maxrange = Promt($"Введите максимальную цифру для образования массива");
+
+int[] array = GenerateArray(length, minrange, maxrange);
+PrintArray(array);
+int[] arr = Invert(array);
+System.Console.WriteLine();
+PrintArray(arr);
+
+
