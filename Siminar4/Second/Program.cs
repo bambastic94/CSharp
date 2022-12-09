537ec9da0cfe2f30b6ecf714906af491a225c612
Dmitriy <Wonderful_days@mail.ru>
2022-12-09 19:07:48 +0300

    Siminar4 add second task

---
+++ Siminar4/Second/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+int Promt(string massage)
+{
+    Console.Write($"{massage} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Multi(int number)
+{
+    int value = 1;
+
+    for (int i = 1; i <= number; i++)
+    {
+        value = value * i;
+    }
+    return value;
+}
+
+int number = Promt("Введите число");
+
+System.Console.WriteLine($"Факториал числа {number} равен {Multi(number)}");
