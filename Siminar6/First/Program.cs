39087f3f5fbb7c050a367743b7feed494f6d9974
Dmitriy <Wonderful_days@mail.ru>
2022-12-16 19:09:43 +0300

    Siminar6 add first task

---
+++ Siminar6/First/Program.cs
@@ -0,0 +1,47 @@
+﻿// Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+
+int Promt(string massage)
+{
+    Console.Write($"{massage} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] GenerateArray(int length, int minrange, int maxrange)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(minrange, maxrange + 1); ;
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        System.Console.Write($"{array[j]}\t");
+    }
+}
+
+int[] ReverseArray(int[] array)
+{
+    int temp = 0;
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        temp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = temp;
+    }
+    return array;
+}
+
+int length = Promt($"Введите длину массива");
+int minrange = Promt($"Введите минимальную цифру для образования массива");
+int maxrange = Promt($"Введите максимальную цифру для образования массива");
+
+int[] array = GenerateArray(length, minrange, maxrange);
+PrintArray(array);
+System.Console.WriteLine();
+int[] arrayReverse = ReverseArray(array);
+PrintArray(arrayReverse);
