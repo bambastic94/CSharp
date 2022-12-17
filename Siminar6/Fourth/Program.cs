16eef52a7bd944bf84b4c208c0a39abb3d6a09f9
Dmitriy <Wonderful_days@mail.ru>
2022-12-17 03:27:19 +0300

    Siminar6 add fourth task

---
+++ Siminar6/Fourth/Program.cs
@@ -0,0 +1,43 @@
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+
+int Promt(string massage)
+{
+    Console.Write($"{massage} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray(int[] array)
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        System.Console.Write($"{array[j]} ");
+    }
+}
+
+int[] Fibonacci(int number)
+{
+    int[] array = new int[number];
+    if (number >= 1) array[0] = 0;
+    if (number >= 2) array[1] = 1;
+    if (number >= 3)
+    {
+        for (int i = 2; i < number; i++)
+        {
+            array[i] = array[i - 1] + array[i - 2];
+        }
+    }
+    return array;
+}
+
+int number = Promt("Какое количество чисел Фибоначчи вывести?");
+
+while (number == 0) 
+{
+    number = Promt("Введите число больше 0");
+}
+
+int[] array = Fibonacci(number);
+
+
+
+PrintArray(array);
