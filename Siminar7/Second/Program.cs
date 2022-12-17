35b33aa3c45f3b8e14fa1a0c4ddb2fedc61de4bb
Dmitriy <Wonderful_days@mail.ru>
2022-12-17 18:48:05 +0300

    Siminar7 add second task

---
+++ Siminar7/Second/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задайте двумерный массив. Найдите элементы, у которых обе позиции чётные, и замените эти элементы на их квадраты.
+
+int Promt(string massage)
+{
+    Console.Write($"{massage} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateDimensionalArray(int rows, int columns, int minrange, int maxrange)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(minrange, maxrange + 1);
+        }
+    }
+    return array;
+}
+
+int[,] ChangedDimensionalArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+                array[i, j] = Convert.ToInt32(Math.Pow(array[i, j], 2));
+        }
+    }
+    return array;
+}
+
+void PrintDimensionalArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int rows = Promt($"Введите количество строк в массиве");
+int columns = Promt($"Введите количество столбцов в массиве");
+int minrange = Promt($"Введите минимальную цифру для образования массива");
+int maxrange = Promt($"Введите максимальную цифру для образования массива");
+
+int[,] array = GenerateDimensionalArray(rows, columns, minrange, maxrange);
+
+PrintDimensionalArray(array);
+System.Console.WriteLine();
+
+int[,] arrayEvenSqr = ChangedDimensionalArray(array);
+
+PrintDimensionalArray(arrayEvenSqr);
