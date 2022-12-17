60b27ff586a13ba257b9a96eb5ea56bdeea30943
Dmitriy <Wonderful_days@mail.ru>
2022-12-17 18:26:40 +0300

    Siminar7 add first task

---
+++ Siminar7/First/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 1: Задайте двумерный массив размера m на n, каждый элемент в массиве находится 
+// по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+
+int Promt(string massage)
+{
+    Console.Write($"{massage} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] SimpleDimensionalArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+    return array;
+}
+
+void PrintDimensionalArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+int rows = Promt($"Введите количество строк в массиве");
+int columns = Promt($"Введите количество столбцов в массиве");
+
+int[,] array = SimpleDimensionalArray(rows, columns);
+
+PrintDimensionalArray(array);
