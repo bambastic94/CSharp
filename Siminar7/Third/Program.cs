d8b8a03a437b525719a4e1934b902f2d44b95f72
Dmitriy <Wonderful_days@mail.ru>
2022-12-17 19:14:39 +0300

    Siminar7 add third task

---
+++ Siminar7/Third/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задайте двумерный массив. Найдите сумму элементов главной диагонали.
+
+int Promt(string massage)
+{
+    Console.Write($"{massage} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateDimensionalArray(int rows, int columns, int minrange, int maxrange)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(minrange, maxrange + 1);
+        }
+    }
+    return array;
+}
+
+int DiagonalArray(int[,] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum += array[i, i];
+    }
+    return sum;
+}
+
+void PrintDimensionalArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int rows = Promt($"Введите количество строк в массиве");
+int columns = Promt($"Введите количество столбцов в массиве");
+int minrange = Promt($"Введите минимальную цифру для образования массива");
+int maxrange = Promt($"Введите максимальную цифру для образования массива");
+
+int[,] array = GenerateDimensionalArray(rows, columns, minrange, maxrange);
+
+PrintDimensionalArray(array);
+System.Console.WriteLine(DiagonalArray(array));
+
