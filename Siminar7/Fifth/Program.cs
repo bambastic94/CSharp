ddb2e8a3a1b7a6c47146bd2ff2df3c3d493ff947
Dmitriy <Wonderful_days@mail.ru>
2022-12-18 02:57:26 +0300

    Siminar7 add fifth task

---
+++ Siminar7/Fifth/Program.cs
@@ -0,0 +1,78 @@
+﻿// Задайте двумерный массив. Найдите максимальный элемент массива и среднее арифметическое всех элементов массива.
+
+
+int Promt(string massage)
+{
+    Console.Write($"{massage} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateDimensionalArray(int rows, int columns, int minrange, int maxrange)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(minrange, maxrange + 1);
+        }
+    }
+    return array;
+}
+
+int FindMaximal(int[,] array)
+{
+    int max = array[0, 0];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] > max)
+            {
+                max = array[i, j];
+            }
+        }
+    }
+    return max;
+}
+
+double SumArray(int[,] array)
+{
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+    }
+    return sum / (array.GetLength(0) * array.GetLength(1));
+}
+
+void PrintDimensionalArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int rows = Promt($"Введите количество строк в массиве");
+int columns = Promt($"Введите количество столбцов в массиве");
+int minrange = Promt($"Введите минимальную цифру для образования массива");
+int maxrange = Promt($"Введите максимальную цифру для образования массива");
+
+
+int[,] array = GenerateDimensionalArray(rows, columns, minrange, maxrange);
+
+PrintDimensionalArray(array);
+
+double sum = SumArray(array);
+int max = FindMaximal(array);
+
+System.Console.WriteLine($"Максимальное значение {max}, а среднее арифметическое {sum}");
