08433204e4b2c6bd5963003ce5c0315411c8cb53
Dmitriy <Wonderful_days@mail.ru>
2022-12-18 04:14:21 +0300

    Homework7 add second task

---
+++ Homework7/Task2/Program.cs
@@ -0,0 +1,63 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает 
+// значение этого элемента или же указание, что такого элемента нет.
+
+int Promt(string massage)
+{
+    Console.Write($"{massage} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateDimensionalArray(int rows, int columns, int minrange, int maxrange)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(minrange, maxrange + 1);
+        }
+    }
+    return array;
+}
+
+int FindElementArray(int[,] array, int i, int j)
+{
+    return array[i, j];
+}
+
+void PrintDimensionalArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+bool IsArray(int[,] array, int i, int j)
+{
+    if (i < array.GetLength(0) || j < array.GetLength(1))
+    {
+        return true;
+    }
+    return false;
+}
+
+int rows = Promt($"Введите количество строк в массиве");
+int columns = Promt($"Введите количество столбцов в массиве");
+int minrange = Promt($"Введите минимальную цифру для образования массива");
+int maxrange = Promt($"Введите максимальную цифру для образования массива");
+
+
+int[,] array = GenerateDimensionalArray(rows, columns, minrange, maxrange);
+PrintDimensionalArray(array);
+
+int i = Promt("Введите строку матрицы");
+int j = Promt("Введите столбец матрицы");
+
+if (IsArray(array, i, j) == true) Console.WriteLine($"Матрица [{i}, {j}] = {FindElementArray(array, i, j)}");
+else Console.WriteLine("По таким значениям данные отсутствуют");
