73cd9c4a481908fa41e748e8b113683608ac7f4b
Dmitriy <Wonderful_days@mail.ru>
2022-12-23 12:46:26 +0300

    Homework8 add first task

---
+++ Homework8/Task1/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int Prompt(string massage)
+{
+    Console.Write($"{massage} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateDimensionalArray(int rows, int columns, int minrange, int maxrange)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(minrange, maxrange + 1);
+        }
+    }
+    return array;
+}
+
+void PrintDimensionalArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] OrderArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int temp = 0;
+        for (int k = 0; k < array.GetLength(1) - 1; k++)
+        {
+            for (int j = 0; j < array.GetLength(1)-1; j++)
+            {
+                if (array[i, j] < array[i, j + 1])
+                {
+                    temp = array[i, j + 1];
+                    array[i, j + 1] = array[i, j];
+                    array[i, j] = temp;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+
+
+int rows = Prompt($"Введите количество строк в массиве");
+int columns = Prompt($"Введите количество столбцов в массиве");
+int minrange = Prompt($"Введите минимальную цифру для образования массива");
+int maxrange = Prompt($"Введите максимальную цифру для образования массива");
+
+
+int[,] array = GenerateDimensionalArray(rows, columns, minrange, maxrange);
+PrintDimensionalArray(array);
+array = OrderArray(array);
+System.Console.WriteLine();
+PrintDimensionalArray(array);
