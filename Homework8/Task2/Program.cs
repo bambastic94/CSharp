2ec4b6619735020a8d30c48368755b0e2df51cb4
Dmitriy <Wonderful_days@mail.ru>
2022-12-23 13:18:07 +0300

    Homework8 add second task

---
+++ Homework8/Task2/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов
+
+int Prompt(string massage)
+{
+    Console.Write($"{massage} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateDimensionalArray(int rows, int columns, int minrange, int maxrange)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(minrange, maxrange + 1);
+        }
+    }
+    return array;
+}
+
+void PrintDimensionalArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+(int, int) FindMinSumRow(int[,] array)
+{
+    int temp = 0;
+    int row = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        if (sum < temp || i == 0)
+        {
+            temp = sum;
+            row = i + 1;
+        }
+    }
+    return (temp, row);
+}
+
+int rows = Prompt($"Введите количество строк в массиве");
+int columns = Prompt($"Введите количество столбцов в массиве");
+int minrange = Prompt($"Введите минимальную цифру для образования массива");
+int maxrange = Prompt($"Введите максимальную цифру для образования массива");
+
+
+int[,] array = GenerateDimensionalArray(rows, columns, minrange, maxrange);
+PrintDimensionalArray(array);
+(int minSumRow, int rowMin) = FindMinSumRow(array);
+System.Console.WriteLine($"В строке {rowMin} наименьшая сумма. Сумма = {minSumRow}");
