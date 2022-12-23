84c6d2238f391b46eff7d2ec3799c09a37cef612
Dmitriy <Wonderful_days@mail.ru>
2022-12-23 15:55:29 +0300

    Homework8 add third task

---
+++ Homework8/Task3/Program.cs
@@ -0,0 +1,105 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+
+int Prompt(string massage)
+{
+    Console.Write($"{massage} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateDimensionalArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void PrintDimensionalArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+bool ConfirmMultiply(int[,] array1, int[,] array2)
+{
+    if (array1.GetLength(0) == array2.GetLength(1) && array1.GetLength(1) == array2.GetLength(0))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int FindDemension(int[,] array)
+{
+    if (array.GetLength(0) <= array.GetLength(1))
+    {
+        return array.GetLength(0);
+    }
+    else
+    {
+        return array.GetLength(1);
+    }
+}
+
+int[,] MultiplyArray(int[,] array1, int[,] array2)
+{
+    int[,] array = new int[FindDemension(array1), FindDemension(array1)];
+    int row = 0;
+    for (int i = 0; i < array1.GetLength(0); i++)
+    {
+        for (int k = 0; k < FindDemension(array1); k++)
+        {
+            for (int j = 0; j < array1.GetLength(1); j++)
+            {
+                if (FindDemension(array1) == array1.GetLength(0))
+                {
+                    array[row, k] += array1[i, j] * array2[j, k];
+                }
+                else
+                {
+                    array[row, k] += array1[j, k] * array2[i, j];
+                }
+            }
+        }
+        row++;
+    }
+    return array;
+}
+
+
+int rows1 = Prompt($"Введите количество строк в первом массиве");
+int columns1 = Prompt($"Введите количество столбцов в первом массиве");
+int[,] array1 = GenerateDimensionalArray(rows1, columns1);
+PrintDimensionalArray(array1);
+
+int rows2 = Prompt($"Введите количество строк во втором массиве");
+int columns2 = Prompt($"Введите количество столбцов во втором массиве");
+int[,] array2 = GenerateDimensionalArray(rows2, columns2);
+PrintDimensionalArray(array2);
+
+if (ConfirmMultiply(array1, array2))
+{
+    int[,] array = MultiplyArray(array1, array2);
+    System.Console.WriteLine("Перемноженная матрица:");
+    PrintDimensionalArray(array);
+}
+else
+{
+    System.Console.WriteLine("Указана неверная размерность");
+}
