1987fbf32ac7fc7900f65dab8043ff7d633b5e4c
Dmitriy <Wonderful_days@mail.ru>
2022-12-25 02:18:49 +0300

    Homework9 add second task

---
+++ Homework9/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+int Prompt(string message)
+{
+    Console.Write($"{message} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int NumeralSum(int M, int N)
+{
+    if (M == N)
+    {
+        return N;
+    }
+    return M + NumeralSum(M + 1, N);
+}
+
+int M = Prompt("Введите первое число");
+int N = Prompt("Введите второе число");
+System.Console.WriteLine($"Сумма чисел в промежутке от {M} до {N} = {NumeralSum(M, N)}");
