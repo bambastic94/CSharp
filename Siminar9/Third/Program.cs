a24f0631d23154b42f8a9b907cdd7f5a2fb856fb
Dmitriy <Wonderful_days@mail.ru>
2022-12-24 20:12:05 +0300

    Siminar9 add third task

---
+++ Siminar9/Third/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр. Использовать рекурсию.
+
+int sum = 0;
+
+int Prompt(string message)
+{
+    Console.Write($"{message} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void SumNumbers(int number)
+{
+    if (number == 0)
+    {
+        return;
+    }
+    sum += number % 10;
+    SumNumbers(number / 10);
+}
+
+// int SumNumbers(int number)
+// {
+//     if (number == 0)
+//     {
+//         return 0;
+//     }
+//     return number % 10 + SumNumbers(number / 10);
+// }
+// System.Console.WriteLine(SumNumbers(Prompt("Введите число > ")));
+
+int number = Prompt("Введите число");
+SumNumbers(number);
+System.Console.WriteLine($"Сумма цифр равна {sum}");
